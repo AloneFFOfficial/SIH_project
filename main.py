3fa76d7218c54b1e6393c5a66cc0380b3062940d
Chaitanya Lohani <109726533+AloneFFOfficial@users.noreply.github.com>
2024-09-22 23:12:07 +0530

    Update main.py
    Fixes some more errors.

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import sqlite3 as sql
 
-'''Geting data from database'''
+'''Getting data from the database'''
 class Database:
     def __init__(self):
         self.conn = sql.connect('Data.db')
-        self.cur = self.conn.cursor() 
+        self.cur = self.conn.cursor()
         data = self.cur.execute('select * from query')
-        self.data = data.fetchall()
+        self.data = data.fetchall()  # Fetch all rows from the 'query' table
         self.conn.close()
-        
-    def __str__(self):
-        return self.data
 
     def get_data(self):
         return self.data
+
 
 '''Creating the query class'''
 class QueryStorage:
@@ -21,7 +19,7 @@ class QueryStorage:
         '''Creating the dict'''
         self.query_storage = {}
 
-    '''Adding all the queries in the dict'''
+    '''Adding a single query to the dict'''
     def add_query(self, query_id, query_text):
         try:
             if query_id in self.query_storage:
@@ -31,7 +29,13 @@ class QueryStorage:
         except ValueError as e:
             print(e)
 
-    '''Vieeing all the queries in the dict'''
+    '''Adding multiple queries from the database'''
+    def add_queries_from_db(self, db_queries):
+        for query in db_queries:
+            query_id, query_text = query  # Unpack each tuple (id, text)
+            self.add_query(query_id, query_text)  # Add each query to the dict
+
+    '''Viewing all the queries in the dict'''
     def view_queries(self):
         if not self.query_storage:
             print("No queries available in memory.")
@@ -39,7 +43,7 @@ class QueryStorage:
             for query_id, query_text in self.query_storage.items():
                 print(f"ID: {query_id}, Query: {query_text}")
 
-    '''Deleting Queries on the basis of its ID from the dict'''
+    '''Deleting queries by ID from the dict'''
     def delete_query(self, query_id):
         try:
             if query_id not in self.query_storage:
@@ -49,7 +53,7 @@ class QueryStorage:
         except KeyError as e:
             print(e)
 
-# Example usage
+
 if __name__ == "__main__":
     '''Calling the query class'''
     query_system = QueryStorage()
@@ -60,11 +64,11 @@ if __name__ == "__main__":
     '''Fetching and adding queries from the Database to QueryStorage'''
     print("\nFetching queries from the database and adding them to memory:")
     db_queries = db.get_data()
-    
+
     if not db_queries:
         print("No queries in the database.")
     else:
-        query_system.add_query(db_queries)
+        query_system.add_queries_from_db(db_queries)
 
     '''Viewing the queries in memory after adding from the database'''
     print("\nStored Queries in Memory after adding from the Database:")
@@ -77,6 +81,3 @@ if __name__ == "__main__":
     print("\nStored Queries in Memory after Deletion:")
     query_system.view_queries()
 
-
-
-    
